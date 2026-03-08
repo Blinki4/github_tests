a6d8353e261b07af97cf566223ea19b12e0b1d87
Blinki4 <dmitriy.velg@mail.ru>
2026-03-08 18:31:28 +0300

    users tests added

--- endpoints/get_user_endpoint.py
+++ endpoints/get_user_endpoint.py
@@ -7,10 +7,10 @@ from endpoints.base_endpoint import BaseEndpoint
 class GetUserEndpoint(BaseEndpoint):
     login: str
 
-    def get_user(self, username: str, authorized: bool = True) -> Response:
+    def get_user(self, username: str) -> Response:
         response = requests.get(
             f'{self.base_url}/users/{username}',
-            headers=self.get_headers(authorized)
+            headers=self.get_headers()
         )
         self.status_code = response.status_code
         if response.status_code == 200:
