39adf18dde0afdafb3b82dc0eae77c6c7dbd9b63
Blinki4 <dmitriy.velg@mail.ru>
2026-04-05 14:08:12 +0300

    Api key rename

--- config/headers.py
+++ config/headers.py
@@ -9,6 +9,6 @@ class Headers:
     def get_headers(self, authorized: bool = True) -> dict[str, str]:
         return {
             'Accept': 'application/vnd.github+json',
-            'Authorization': f'Bearer {os.getenv("TOKEN")}' if authorized else None,
+            'Authorization': f'Bearer {os.getenv("API_KEY")}' if authorized else None,
             'X-GitHub-Api-Version': '2026-03-10',
         }
