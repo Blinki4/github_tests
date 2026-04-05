76699ccded63994305b4555d31876d6dc7e0477b
Blinki4 <dmitriy.velg@mail.ru>
2026-04-05 12:21:49 +0300

    secrets added

--- tests/api/test_repositories_api.py
+++ tests/api/test_repositories_api.py
@@ -66,7 +66,7 @@ class TestRepositoriesAPI:
 
     @pytest.mark.api
     @allure.title('API удаление репозитория')
-    def test_delete_repository(self, create_repository_req, repositories_api_service):
+    def test_delete_repository(self, create_repository_req, repositories_api_service, delete_repository_req):
         response = repositories_api_service.delete_repository(
             owner=os.getenv('LOGIN'),
             repo=os.getenv('REPO_NAME')
