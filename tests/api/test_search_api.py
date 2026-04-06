53208c0ba9050ee95bdde8465034dc82431d8657
Blinki4 <dmitriy.velg@mail.ru>
2026-04-06 22:58:37 +0300

    new test

--- tests/api/test_search_api.py
+++ tests/api/test_search_api.py
@@ -26,6 +26,10 @@ class TestSearchAPI:
 
     @pytest.mark.api
     @allure.title('Поиск по несуществующему запросу')
-    def test_search_non_existing_query_api(self, search_api_service):
+    def test_search_repository_non_existing_query_api(self, search_api_service):
         response = search_api_service.search_repository(query='lkasjdljadhskdfhgksdjgfsdfkghj')
         assert response.total_count == 0
+
+    def test_search_repository_for_language(self, search_api_service):
+        response = search_api_service.search_repository(query='language:python')
+        assert response.total_count > 0
